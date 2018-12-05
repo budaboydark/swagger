7cb4d41756c7c0a9c8357a68962d6d44b05e747a
Rodrigo Silva <rodrigo@rodrigosilva.user.f1s.me>
2018-12-05 11:22:07 -0200

    exemplo de swagger

---
+++ api/api.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @OA\Info(version="1.0.0", title="Api",description="Documentação API")
+ * @OA\Server(url="http://192.168.0.44:8080/f1webservices/public/api")
+ */
+
+/**
+ * @OA\Get(
+ *     path="/api/resource.json",
+ *     @OA\Response(response="200", description="An example resource")
+ * )
+ */
